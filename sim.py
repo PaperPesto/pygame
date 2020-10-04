c2c56214a3c0a02677e2e9d4a6d8db1dd163e447
Leonardo Bianchini <leonardo.bianchini90@gmail.com>
2020-10-04 16:28:53 +0200

    log e popup message

--- sim.py
+++ sim.py
@@ -5,6 +5,7 @@ import math
 import uuid
 
 pygame.init()
+pygame.display.set_caption('sim')
 
 WIDTH = 800
 HEIGHT = 600
@@ -15,6 +16,9 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255,255, 0)
 BACKGROUND_COLOR = (0,0,0)
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
 
 DEFAULT_SIZE = 10
 
@@ -22,8 +26,8 @@ clock = pygame.time.Clock()
 
 class Entity:
 
-    def __init__(self, id, x = 0, y = 0, vx = 0, vy = 0, size = DEFAULT_SIZE, color = (255, 255, 255)):
-        self.id = id
+    def __init__(self, uuid, x = 0, y = 0, vx = 0, vy = 0, size = DEFAULT_SIZE, color = (255, 255, 255)):
+        self.uuid = uuid
         self.x = x
         self.y = y
         self.vx = vx
@@ -41,7 +45,10 @@ class Entity:
         self.__move()
 
     def log(self):
-        print(str(self.id) + ' ' + 'x: ' + str(self.x) + ' y: ' + str(self.y))
+        return self.id() + " " + "x: " + str(self.x) + " y: " + str(self.y)
+
+    def id(self):
+        return str(self.uuid[0:4])
 
     def __move(self):
         self.x = self.x + self.vx
@@ -49,6 +56,8 @@ class Entity:
 
 
 entity_list = []
+selected_entity = None
+show_popup = False  # flag per visualizzazione del popup delle statistiche dell'entità
 
 def init_entities(entity_number = 10):
     for i in range(entity_number):
@@ -62,24 +71,47 @@ def init_entities(entity_number = 10):
         e = Entity(id, x, y, vx, vy, size, color)
         entity_list.append(e)
 
+def search_entities(x, y):
+    for e in entity_list:
+        if (x >= e.x and x < e.x + e.width) and (y >= e.y and y < e.y + e.height):
+            return e
+    return None
+
 init_entities()
 
 while True:
+
+    #screen.fill(BACKGROUND_COLOR)
 
     for event in pygame.event.get():
         print(event)
 
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            e = search_entities(event.pos[0], event.pos[1])
+            if e:
+                print('found ' + e.id())
+                show_popup = True
+                selected_entity = e
+            else:
+                show_popup = False
+
 
     screen.fill(BACKGROUND_COLOR)
 
     for e in entity_list:
         e.draw()
         e.act()
-        e.log()
+        #print(e.log())
+    
+    if show_popup:
+        font = pygame.font.SysFont("monospace", 35)
+        text = font.render(selected_entity.log(), True, green, blue)
+        text_rect = text.get_rect()
+        text_rect.center = (400, 300)
+        screen.blit(text, text_rect)
 
-    clock.tick(1)
-    #print('hello tick: ' + str(pygame.time.get_ticks()))
+    clock.tick(30)
 
     pygame.display.update()
