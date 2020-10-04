bc4d94e6e88da9734a1ce9f4fe67c0e72651639e
Leonardo Bianchini <leonardo.bianchini90@gmail.com>
2020-10-04 12:50:12 +0200

    aggiunto enemy

--- game.py
+++ game.py
@@ -1,10 +1,22 @@
 import pygame
 import sys
 
+# doc https://www.youtube.com/watch?v=-8n91btt5d8&ab_channel=KeithGalli
+
 pygame.init()
 
 WIDTH = 800
 HEIGHT = 600
+
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BACKGROUND_COLOR = (0,0,0)
+
+player_size = 50
+player_pos = [WIDTH/2, HEIGHT - 2*player_size]
+
+enemy_size = 50
+enemy_pos = [100, 0]
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -14,3 +26,25 @@ while not game_over:
 
     for event in pygame.event.get():
         print(event)
+
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+
+            x = player_pos[0]
+            y = player_pos[1]
+
+            if event.key == pygame.K_LEFT:
+                x -= player_size
+            elif event.key == pygame.K_RIGHT:
+                x += player_size
+
+            player_pos = [x, y]
+
+    screen.fill(BACKGROUND_COLOR)
+
+    pygame.draw.rect(screen, BLUE, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
+    pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
+
+    pygame.display.update()
