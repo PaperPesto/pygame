f78ab613270851540716a27fce5233eef4baefa8
Leonardo Bianchini <leonardo.bianchini90@gmail.com>
2020-10-04 13:05:11 +0200

    27:59 - Setting the frames per second rate (FPS)/Controlling speed of falling blocks.
    Manca il collision detection

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+import random
 
 # doc https://www.youtube.com/watch?v=-8n91btt5d8&ab_channel=KeithGalli
 
@@ -12,15 +13,19 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 BACKGROUND_COLOR = (0,0,0)
 
+SPEED = 10
+
 player_size = 50
 player_pos = [WIDTH/2, HEIGHT - 2*player_size]
 
 enemy_size = 50
-enemy_pos = [100, 0]
+enemy_pos = [random.randint(0, WIDTH - enemy_size), 0]
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 game_over = False
+
+clock = pygame.time.Clock()
 
 while not game_over:
 
@@ -44,7 +49,15 @@ while not game_over:
 
     screen.fill(BACKGROUND_COLOR)
 
+    if enemy_pos[1] >= 0 and enemy_pos[1] < HEIGHT:
+        enemy_pos[1] += SPEED
+    else:
+        enemy_pos[0] = random.randint(0, WIDTH - enemy_size)
+        enemy_pos[1] = 0
+
     pygame.draw.rect(screen, BLUE, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
     pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
+
+    clock.tick(60)
 
     pygame.display.update()
